b633f424241f309b82324185a5be5c3bb87b8a01
mdlincoln <matthew.d.lincoln@gmail.com>
2013-08-07 11:24:15 -0400

    Scrapes topics from collections.si.edu

--- scrape.rb
+++ scrape.rb
@@ -1,20 +1,31 @@
 require 'nokogiri'
 require 'csv'
 
-INPUT = "sample.html"
+INPUT = "si.html"
 OUTPUT = "out.csv"
 
 ######### Initialize CSV #########
 
 csv_out = CSV.open(OUTPUT, "w")
+csv_out << ["title","keywords"]
 
 ######### Parse HTML #########
 
 puts "Parsing HTML..."
-sample = Nokogiri::HTML(open(INPUT))
+sample = Nokogiri::HTML(open(INPUT)) do |config|
+	config.noblanks
+end
 
-sample.css("#contentArtworkKeywords p").each do |keyword|
-	string = keyword.content
-	string.gsub!(/\n/," ").gsub!("  "," ").strip!
-	puts string
+sample.css("div .record").each do |record|
+
+	# Record info
+	title = record.at_css("h2.title").content
+	keywords = Array.new
+
+	# Get keywords
+	keywords << record.at_css("dd.topic-first").content
+	record.css("dd.topic").each do |subtopic|
+		keywords << subtopic.content
+	end
+	csv_out << [title,keywords]
 end
