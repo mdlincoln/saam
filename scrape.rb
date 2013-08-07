793308ba6f409d4fa2bd11e7f70dbbe9fdbee5d2
mdlincoln <matthew.d.lincoln@gmail.com>
2013-08-07 13:20:54 -0400

    Read all the collections
    Assigns soruce, target, and date of painting form which the edge was
    defined

--- scrape.rb
+++ scrape.rb
@@ -16,7 +16,7 @@ OUTPUT = "out.csv"
 ######### Initialize CSV #########
 
 csv_out = CSV.open(OUTPUT, "w")
-csv_out << ["source","target"]
+csv_out << ["source","target","date"]
 
 index = 0
 
@@ -29,20 +29,25 @@ loop do
 		config.noblanks
 	end
 
-	sample.css("div .record").each do |record|
+	sample.css("div.record").each do |record|
 
-		keywords = Array.new
-
-		# Get keywords
-		puts
-		print "Keyword combos: "
-		keywords << getContent(record.at_css("dd.topic-first"))
-		record.css("dd.topic").each do |subtopic|
-			keywords << getContent(subtopic)
-		end
-		keywords.combination(2).each do |array|
-			print "#"
-			csv_out << array
+		raw_date = getContent(record.at_css("dd.date-first"))
+		if raw_date.nil?
+			# Skip and go to next record
+		else
+			date = raw_date.slice(/(\d*)(\D|$)/,1)
+			keywords = Array.new
+			# Get keywords
+			puts
+			print "#{date} Keyword combos: "
+			keywords << getContent(record.at_css("dd.topic-first"))
+			record.css("dd.topic").each do |subtopic|
+				keywords << getContent(subtopic)
+			end
+			keywords.combination(2).each do |array|
+				print "#"
+				csv_out << [array[0],array[1],date]
+			end
 		end
 	end
 	sleep 2
