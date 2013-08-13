f0cb69f822b21a886a6c7552dcfc9e6d3be484a3
mdlincoln <matthew.d.lincoln@gmail.com>
2013-08-13 14:13:45 -0400

    Change terminology from "normalized" to "cleaned"

--- normalize.rb
+++ normalize.rb
@@ -5,16 +5,16 @@ require 'json'
 ######### Private methods #########
 
 #  Normalize dates, e.g. "c. 1865" becomes "1865", and "1901-1902" becomes "1902".
-def normalizeDate(input)
 	if input.nil?
+def cleanDate(input)
+	cleaned = input.first.slice(/(\d*)(\D|$)/,1).to_i
 		return nil
 	else
-		normalized = input.first.slice(/(\d*)(\D|$)/,1).to_i
 		if normalized == 0
 			return nil
 		else
-			return normalized
 		end
+		return cleaned
 	end
 end
 
@@ -34,7 +34,7 @@ end
 INPUT = "output.json"
 OUTPUT = "normalized_saam_two_topics.json"
 raw_data = JSON.parse(File.read(INPUT))
-normalized_data = Hash.new
+cleaned_data = Hash.new
 
 puts "Number of raw records: #{raw_data.count}"
 
@@ -43,32 +43,32 @@ puts "Cleaning records"
 # Only including records that have both "Date" and "Topic"
 raw_data.select {|k,v| v.has_key?("Date") && v.has_key?("Topic")}.each do |id, data|
 	
-	# Normalize date
-	normalized_date = normalizeDate(data["Date"])
+	# Clean date
+	clean_date = cleanDate(data["Date"])
 
-	# Normalize topics
 	normalized_topics = Array.new
 	data["Topic"].each do |topic|
 		normalized_topics << cleanTopic(topic)
 	end
 	
+	# Clean topics
 	# Check that normalized values are still valid, e.g. items that had a Date value of "n.d" will no longer be valid after cleaning, and must be discarded
-	if normalized_date.nil?
+	if clean_date.nil?
 		# Skips record
 	else
 		# Replace original values with normalized values, and store in normalized_data hash
-		data["Date"] = normalized_date
-		data["Topic"] = normalized_topics
-		normalized_data[id] = data
+		data["Date"] = clean_date
+		data["Topic"] = clean_topics
+		cleaned_data[id] = data
 	end
 end
 
 ######### Write JSON file #########
 
-puts "Number of normalied records: #{normalized_data.count}"
+puts "Number of normalied records: #{cleaned_data.count}"
 
 puts "Writing JSON..."
 File.open(OUTPUT,"w") do |file|
-	file.write(JSON.pretty_generate(normalized_data))
+	file.write(JSON.pretty_generate(cleaned_data))
 end
 puts "Finished."
