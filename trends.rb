2444ef4c9b28ea3614d9f41a15c0272d1ee540a8
mdlincoln <matthew.d.lincoln@gmail.com>
2013-08-13 16:15:12 -0400

    Clarify comments

--- trends.rb
+++ trends.rb
@@ -1,8 +1,10 @@
 require 'json'
 require 'csv'
 
+######### Define arguments #########
+
 INPUT = "data/cleaned.json"
-TARGET = "figure(s) in interior"
+TARGET = "figure(s) in interior" # Enter keyword to be profiled
 OUTPUT = "data/#{TARGET}.csv"
 
 puts "Loading data..."
@@ -10,21 +12,30 @@ data = JSON.parse(File.read(INPUT))
 trends = CSV.open(OUTPUT,"w")
 trends << ["year","total paintings","topic paintings", TARGET]
 
-step = 1800
+step = 1800 # Enter the year to begin (default to 1800, as this is when SAAM collections begin to get interesting)
+
+######### Check each year #########
 
 puts "Checking all years since #{step} for `#{TARGET}`"
 while step < 2012
+
+	# How many total records have that year?
 	total = data.select{|k,v| v["Date"]==step}.count.to_f
+
+	# How many total records from that year have the target topic?
 	with_topic = data.select{|k,v| v["Date"]==step && v["Topic"].include?(TARGET)}.count.to_f
 	
+	# Calculate the ratio of ptgs with topic to total paintings that year
 	if total == 0
 		ratio = 0
 	else
 		ratio = with_topic / total
 	end
 
+	# Write out row to CSV
 	trends << [step,total,with_topic,ratio]
 
+	# Increment to the next year
 	step += 1
 end
 
