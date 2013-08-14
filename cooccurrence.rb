16ffd3ae8bd8d3bbbe0fd334aefbfe2d254c56ac
mdlincoln <matthew.d.lincoln@gmail.com>
2013-08-14 14:47:02 -0400

    Clarifying comments and stdout messages

--- cooccurrence.rb
+++ cooccurrence.rb
@@ -3,11 +3,23 @@ require 'json'
 
 INPUT = "data/JSON/cleaned.json"
 QUERY = JSON.parse(File.read("query.json"), :symbolize_names => true)
+puts QUERY
 
+# query.json should be written as follows:
+
+# [
+# 	{
+# 		"begin": 1750,		=> Lower bound
+# 		"end": 1750			=> Upper bound
+# 	},... => Additional queries follow this
+# ]
+
+puts "Loading data..."
 raw_data = JSON.parse(File.read(INPUT))
 first_pass = Array.new
 
 # Create full edge list
+puts "Creating edge list..."
 raw_data.each do |id, data|
 	date = data["Date"]
 
@@ -20,15 +32,18 @@ end
 
 
 # Write out query edge lists
+puts "Writing CSV files..."
 list_header = ["source","target","label","type","date"]
 QUERY.each do |v|
 	start_date = v[:begin]
 	end_date = v[:end]
 	filename = "#{start_date}-#{end_date}.csv"
+	print "Writing #{filename}..."
 	output = CSV.open("data/networks/test/#{filename}","w")
 	output << list_header
 
 	first_pass.select{ |entry| entry[4].between?(start_date,end_date) }.each do |line|
 		output << line
 	end
+	puts "done."
 end
