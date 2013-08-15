1831b46aeeee1ff1b01d0b7a6e38c20009779c0a
mdlincoln <matthew.d.lincoln@gmail.com>
2013-08-15 18:14:30 -0400

    Pre-filter edges by evaluating weight in Ruby

--- topicmatch.rb
+++ topicmatch.rb
@@ -12,26 +12,35 @@ count = raw_data.count
 node_list = CSV.open(OUT_NODES,"w")
 node_list << ["id","label","date"]
 edge_list = CSV.open(OUT_EDGES,"w")
-edge_list << ["source","target","label","type"]
+edge_list << ["source","target","weight","type"]
 
+id_list = Array.new
 
 # Create full node list
 puts "Creating node list..."
 raw_data.each do |id, data|
+	id_list << id
 	date = data["Date"]
 	title = data["Title"]
 	node_list << [id,title,date]
 end
 
-prog_bar = ProgressBar.create(:title => "records connected", :starting_at => 0, :total => count, :format => '|%b>>%i| %p%% %t')	# => Create a progress bar
 
-puts "Creating edge list..."
-raw_data.each do |id, data|
-	data["Topic"].each do |topic|
-		raw_data.select{|k,v| v["Topic"].include?(topic)}.each do |key, value|
-			 edge_list << [id,key,topic,"Undirected"]
-		end
+puts "Calculating potential node combinations..."
+combos = id_list.combination(2)
+
+prog_bar = ProgressBar.create(:title => "records connected", :starting_at => 0, :total => combos.count, :format => '|%b>>%i| %p%% %t')	# => Create a progress bar
+
+puts "Evaluating combinations..."
+combos.each do |pair|
+	id_one = pair[0]
+	id_two = pair[1]
+	topics_one = raw_data[id_one]["Topic"]
+	topics_two = raw_data[id_two]["Topic"]
+	weight = (topics_one & topics_two).count
+	if weight > 0
+		edge_list << [id_one,id_two,weight,"Undirected"]
+	else
 	end
 	prog_bar.increment
 end
-
